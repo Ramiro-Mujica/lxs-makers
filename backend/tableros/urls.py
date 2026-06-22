de4756ea31d0f96eab4c47d644c522d68f5e3786
Uziel Mujica <uziel.mujica@davinci.edu.ar>
2026-06-22 05:54:39 -0300

    Delete backend directory

--- backend/tableros/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',                                              views.tableros,        name='tableros'),
-    path('<uuid:tablero_id>/',                            views.tablero_detalle, name='tablero_detalle'),
-    path('<uuid:tablero_id>/tareas/',                     views.agregar_tarea,   name='agregar_tarea'),
-    path('<uuid:tablero_id>/tareas/<uuid:tarea_id>/',     views.tarea_detalle,   name='tarea_detalle'),
-]
